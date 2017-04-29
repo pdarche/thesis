e5944eff8f2d0b24ff4cad92b8a8db9cac14e2a1
Peter Darche <pdarche@gmail.com>
2017-04-29 13:22:32 -0400

    Improve munging, add more features to dataset, mess with models

--- code/Prosocial Spon Coop .R
+++ code/Prosocial Spon Coop .R
@@ -5,13 +5,18 @@ library(readstata13)
 library(foreign)
 library(arm)
 
-data <- read.dta13('../data/spon_coop_for_prosocials.dta')
+data <- read.dta13('../data/thirdparty/spon_coop_for_prosocials.dta')
 names(data)
 head(data$svoangle)
 
-hist(data$con_all)
+
+
 plot(data$svoangle, data$con_all)
+
+write.csv(data, file = "~/Desktop/qmss/spring17/thesis/data/thirdparty/spon_coop_prosocials.csv")
 
 fit <- lm(data$con_all~data$svoangle) 
 summary(fit)
 coef(fit)
+
+
